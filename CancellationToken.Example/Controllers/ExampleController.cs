2bb0e656da63449814b9a06085a5a70424b97030
markodosen <marko.dosen@infinum.com>
2022-12-09 13:34:28 +0100

    Initial commit.

---
+++ CancellationToken.Example/Controllers/ExampleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CancellationToken.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExampleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet("a")]
+        public async Task<IActionResult> GetA()
+        {
+            var result = await _httpClient.GetAsync("https://localhost:7164/api/external");
+
+            var content = await result.Content.ReadAsStringAsync();
+
+            return Ok(content);
+        }
+
+        [HttpGet("b")]
+        public async Task<IActionResult> GetB()
+        {
+            System.Threading.CancellationToken token = HttpContext.RequestAborted;
+            var result = await _httpClient.GetAsync("https://localhost:7164/api/external", token);
+
+            var content = await result.Content.ReadAsStringAsync();
+
+            return Ok(content);
+        }
+    }
+}
---
+++ CancellationToken.Example/Controllers/ExternalController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CancellationToken.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExternalController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Thread.Sleep(new TimeSpan(0, 0, 5));
+
+            return Ok("Hello World");
+        }
+    }
+}
